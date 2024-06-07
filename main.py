ca57c65e3f1cf6beafe20406a5eb8cec389acae0
MaxUhl98 <frozen2ice@web.de>
2024-06-07 15:03:48 +0200

    feat: renamed playground to improve understandability

---
+++ main.py
@@ -0,0 +1,12 @@
+import torch
+from demo_configuration import DemonstrationConfig
+from classification.demonstrate import demonstrate
+if __name__ == '__main__':
+    torch.set_default_device('cpu')
+    cfg = DemonstrationConfig()
+    demonstrate(cfg)
+
+
+
+
+
