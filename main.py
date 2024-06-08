400752385128a775d2efcb377ef98127c9385d35
MaxUhl98 <frozen2ice@web.de>
2024-06-08 08:20:54 +0200

    changed layout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import torch
 from demo_configuration import DemonstrationConfig
 from classification.demonstrate import demonstrate
+
 if __name__ == '__main__':
     torch.set_default_device('cpu')
     cfg = DemonstrationConfig()
