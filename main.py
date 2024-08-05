540aef566169922de99da7242255ab798681f943
MaxUhl98 <frozen2ice@web.de>
2024-08-05 08:47:31 +0200

    polished main

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-import torch
 from demo_configuration import DemonstrationConfig
-from classification.demonstrate import demonstrate
+from classification import demonstrate, train_model
 
-if __name__ == '__main__':
-    torch.set_default_device('cpu')
+
+def train_and_demonstrate() -> None:
+    """Trains models using the configuration and demonstrates the results after training
+
+    :return: None (writes training information to the used model logfile, saves models in the model folder.
+    Also visualizes results in a confusion matrix and via showing labeled samples)
+    """
+    cfg = DemonstrationConfig()
+    train_model(cfg)
+    demonstrate(cfg)
+
+
+def run_demonstration() -> None:
+    """Runs the demonstration with the existing configured models.
+
+    :return: None (Visualizes results in a confusion matrix and via showing labeled samples)
+    """
     cfg = DemonstrationConfig()
     demonstrate(cfg)
 
 
-
-
-
+if __name__ == '__main__':
+    run_demonstration()
