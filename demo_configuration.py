fe71dfbb27510aac13a83b124e5dfbc6bcc6b610
MaxUhl98 <frozen2ice@web.de>
2024-08-05 08:01:13 +0200

    reorganized configuration, added new params in docstring

--- demo_configuration.py
+++ demo_configuration.py
@@ -1,53 +1,84 @@
+from typing import *
+from torchvision.transforms import v2
 import os
-from typing import Any, Union
+from utils import get_label_mapping_dictionary
 
 
 class DemonstrationConfig:
-    model_name: str = 'ConvNeXt_V2'
+    """
+    Configuration class for model training and testing.
 
-    # Train Hyperparameters
-    num_folds: int = 5
-    batch_size: int = 16
-    num_epochs: int = 50
-    patience: int = 1
-    learning_rate: float = 10 ** -5
-    max_grad_norm: [int, float] = 10 ** 5
-    gradient_accumulation_steps: int = 1
+    :param model_name: The name of the model to be used.
+    :param num_folds: The number of folds for cross-validation.
+    :param shuffle_folds: Whether to shuffle data before creating folds.
+    :param batch_size: The batch size for training.
+    :param num_epochs: The number of epochs for training.
+    :param patience: The number of epochs to wait for improvement before early stopping.
+    :param learning_rate: The learning rate for the optimizer.
+    :param max_grad_norm: The maximum gradient norm for gradient clipping.
+    :param apex: Whether to use Apex for mixed precision training.
+    :param train_data_paths: List of paths to directories containing training image folders.
+    :param test_data_paths: List of paths to directories containing test image folders.
+    :param use_reduced_dataset: Whether to use a reduced dataset for training.
+    :param reduced_percentage: The percentage of the dataset to use if `use_reduced_dataset` is True.
+    :param augmentations: The data augmentation transforms to be applied during training.
+    :param save_best: Whether to save the best model based on validation performance.
+    :param model_save_path: The path where the model will be saved.
+    :param lr_scheduling: Whether to use learning rate scheduling.
+    :param t_0: The number of epochs before the first restart in learning rate scheduling.
+    :param log_dir: Directory where training logs will be saved.
+    :param log_batch_loss: Whether to log the loss for each batch.
+    :param model_input_size: The input size of the model in (channels, height, width) format.
+    :param class_mappings: A dictionary mapping class names to integer labels.
+    """
+    model_name: str = 'MiniVGG'
 
-    model_input_size: tuple[int, int, int] = (3, 224, 224)
+    # Cross Validation settings
+    num_folds: int = 10
+    shuffle_folds: bool = True
 
-    # Paths to directories containing train image folders
-    train_data_paths: list[Union[str, os.PathLike]] = [
-        r'classification\data\Fruit And Vegetable Diseases Dataset\train']
-    # Paths to directories containing test image folders
-    test_data_path: list[Union[
-        str, os.PathLike]] = [
-        r'C:classification\data\Fruit And Vegetable Diseases Dataset\test']
     # Train Settings
+    batch_size: int = 32
+    num_epochs: int = 100
+    patience: int = 5
+    learning_rate: float = 10 ** -4
+    max_grad_norm: Union[int, float] = 10 ** 5
+    apex: bool = False
+
+    # Data paths
+    train_data_paths: list[Union[str, os.PathLike]] = [
+        r'classification/data/Eye_Disease_Detection/train/train']
+    test_data_paths: list[Union[str, os.PathLike]] = [
+        r'classification/data/Eye_Disease_Detection/validation/validation']
+
+    # Data Settings
+    use_reduced_dataset: bool = False
+    reduced_percentage: float = .1
+    augmentations = v2.Compose([v2.AutoAugment()])
+
+    # Weight saving settings
     save_best: bool = True
     model_save_path: str = f'classification/models/trained_models/{model_name}/{model_name}.pth'
-    shuffle_folds: bool = True
-    apex: bool = False
-    lr_scheduling: bool = False
 
-    # Logging Settings
+    # LR Scheduling
+    lr_scheduling: bool = True
+    t_0: int = 3
+
+    # Logging settings
     log_dir: Union[str, os.PathLike] = 'classification/models/training_logs'
     log_batch_loss: bool = False
 
-    class_mappings: dict[str, int] = {'Apple__Healthy': 0, 'Apple__Rotten': 1, 'Banana__Healthy': 2,
-                                      'Banana__Rotten': 3, 'Bellpepper__Healthy': 4, 'Bellpepper__Rotten': 5,
-                                      'Carrot__Healthy': 6, 'Carrot__Rotten': 7, 'Cucumber__Healthy': 8,
-                                      'Cucumber__Rotten': 9, 'Grape__Healthy': 10, 'Grape__Rotten': 11,
-                                      'Guava__Healthy': 12, 'Guava__Rotten': 13, 'Jujube__Healthy': 14,
-                                      'Jujube__Rotten': 15, 'Mango__Healthy': 16, 'Mango__Rotten': 17,
-                                      'Orange__Healthy': 18, 'Orange__Rotten': 19, 'Pomegranate__Healthy': 20,
-                                      'Pomegranate__Rotten': 21, 'Potato__Healthy': 22, 'Potato__Rotten': 23,
-                                      'Strawberry__Healthy': 24, 'Strawberry__Rotten': 25, 'Tomato__Healthy': 26,
-                                      'Tomato__Rotten': 27}
-
+    # Only touch if you know what you are doing
+    model_input_size: tuple[int, int, int] = (3, 224, 224)
+    class_mappings: dict[str, int] = get_label_mapping_dictionary(train_data_paths[0])
 
     @staticmethod
     def get_efficientnet_kwargs() -> dict[str, Any]:
+        """
+        Get the keyword arguments for the EfficientNet model.
+
+        :return: A dictionary of keyword arguments for the EfficientNet model.
+        """
         return {
             'model_name': "tf_efficientnetv2_s.in1k",
             'pretrained': True,
@@ -56,16 +87,31 @@ class DemonstrationConfig:
 
     @staticmethod
     def get_vit_kwargs() -> dict[str, Any]:
+        """
+        Get the keyword arguments for the Vision Transformer (ViT) model.
+
+        :return: A dictionary of keyword arguments for the ViT model.
+        """
         return {
             'model_name': "vit_tiny_patch16_224.augreg_in21k_ft_in1k",
             'pretrained': True}
 
     @staticmethod
     def get_convnextv2_kwargs() -> dict[str, Any]:
+        """
+        Get the keyword arguments for the ConvNeXtV2 model.
+
+        :return: A dictionary of keyword arguments for the ConvNeXtV2 model.
+        """
         return {
             'model_name': "convnextv2_pico.fcmae_ft_in1k",
             'pretrained': True}
 
     @staticmethod
     def get_minivgg_arg() -> int:
+        """
+        Get the number of Input Channels for miniVGG.
+
+        :return: Number of input channels for the MiniVGG model.
+        """
         return 3
