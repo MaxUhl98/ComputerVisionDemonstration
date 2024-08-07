41ba3fc50c3f71b9d19b5da32a56f72031d85afd
MaxUhl98 <frozen2ice@web.de>
2024-08-07 15:04:57 +0200

    changed final transfer learning layers

--- classification/models/ViT.py
+++ classification/models/ViT.py
@@ -1,4 +1,5 @@
 import timm
+import torch
 from torch import nn
 from typing import *
 
@@ -6,6 +7,7 @@ from typing import *
 class CustomizedViT(nn.Module):
     """ViT model with a customized head. Has the option to be loaded with pretrained weights.
         Model architecture is based on https://arxiv.org/pdf/2010.11929"""
+
     def __init__(self, model_kwargs: Dict[str, Any], freeze: bool = False,
                  num_frozen_layers: int = 4, num_classes=28):
         super(CustomizedViT, self).__init__()
@@ -15,7 +17,8 @@ class CustomizedViT(nn.Module):
         if freeze:
             for i, (name, param) in enumerate(list(self.model.named_parameters())[0:num_frozen_layers]):
                 param.requires_grad = False
-        model = list(self.model.children())[:-1] + [nn.Flatten(), nn.ReLU(), nn.Linear(37632, num_classes)]
+        model = list(self.model.children())[:-2] + [nn.AdaptiveAvgPool2d(64), nn.Flatten(), nn.Dropout(0.1),
+                                                    nn.Linear(4096, num_classes)]
         self.model = nn.Sequential(*model)
 
     def forward(self, x):
